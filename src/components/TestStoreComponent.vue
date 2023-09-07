2df83ddad9f9b0109a51480763d3a6dbd645106a
ZhenyaGro <bon4ios@gmail.com>
2023-09-07 22:47:41 +0300

    Test store

---
+++ src/components/TestStoreComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{ counter }}</p>
+    <button @click="increase">+</button>
+    <button @click="decrease">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestStore',
+  computed: {
+    counter() {
+      return this.$store.state.count;
+    }
+  },
+  methods: {
+    increase() {
+      this.$store.commit('increment');
+    },
+    decrease() {
+      this.$store.commit('decrement');
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createStore } from 'vuex';
 
-createApp(App).mount('#app')
+const store = createStore({
+  state() {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+    decrement(state) {
+      state.count--;
+    }
+  }
+})
+
+createApp(App)
+  .use(store)
+  .mount('#app');
